4fcbf63c3bd592e28e21e4d69459aeb94cef8582
Christoffer Trebo <christoffer.trebo@gmail.com>
2022-09-24 16:14:29 +0200

    migrate to postgressql

--- djBugTracker/djBugTracker/settings.py
+++ djBugTracker/djBugTracker/settings.py
@@ -94,12 +94,14 @@ WSGI_APPLICATION = 'djBugTracker.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',# here I also saw postgres_psycopg2
+    'NAME': 'bugTracker1',
+    'USER': str(os.getenv('DB_USER')),
+    'PASSWORD': str(os.getenv('DB_PW')),
+    'HOST': 'localhost',
+    'PORT': '5432',
+}}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
